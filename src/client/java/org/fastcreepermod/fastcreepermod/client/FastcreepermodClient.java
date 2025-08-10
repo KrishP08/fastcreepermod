7022e0fe0b64033e056261695640186716c4491c
KrishP08 <7233kp@gmail.com>
2025-08-10 14:10:56 +0530

    commit

---
+++ src/client/java/org/fastcreepermod/fastcreepermod/client/FastcreepermodClient.java
@@ -0,0 +1,10 @@
+package org.fastcreepermod.fastcreepermod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FastcreepermodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/fastcreepermod/fastcreepermod/Fastcreepermod.java
@@ -0,0 +1,10 @@
+package org.fastcreepermod.fastcreepermod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fastcreepermod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
