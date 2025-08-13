26d31910c49a12cbec51598008823462315a22ff
KrishP08 <7233kp@gmail.com>
2025-08-14 00:40:38 +0530

    add new effect

--- src/main/java/org/fastcreepermod/fastcreepermod/BismithIngot.java
+++
@@ -1,9 +0,0 @@
-package org.fastcreepermod.fastcreepermod;
-
-import net.minecraft.item.Item;
-
-public class BismithIngot extends Item {
-    public BismithIngot() {
-        super(new Item.Settings().maxCount(64));
-    }
-}
--- src/main/java/org/fastcreepermod/fastcreepermod/RawBismith.java
+++
@@ -1,9 +0,0 @@
-package org.fastcreepermod.fastcreepermod;
-
-import net.minecraft.item.Item;
-
-public class RawBismith extends Item {
-    public RawBismith() {
-        super(new Item.Settings().maxCount(64));
-    }
-}
