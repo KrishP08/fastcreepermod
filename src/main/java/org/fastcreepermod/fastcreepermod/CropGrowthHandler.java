c28904ab3e2230118b8961d06e9d543f802297ff
KrishP08 <7233kp@gmail.com>
2025-08-14 00:35:28 +0530

    add new effect

--- src/main/java/org/fastcreepermod/fastcreepermod/CropGrowthHandler.java
+++ src/main/java/org/fastcreepermod/fastcreepermod/CropGrowthHandler.java
@@ -12,70 +12,96 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class CropGrowthHandler {
 
     // --- Configuration ---
     private static final int GROWTH_RADIUS = 4;
-    // Reduced chance to make growth a bit slower overall
     private static final int GROWTH_CHANCE = 15;
-    // The chance for the "special" event to happen
-    private static final float FART_CHANCE = 0.45f;
+    // The chance for the "special" event to happen, now 30%
+    private static final float FART_CHANCE = 0.30f;
+    // Timer duration: 20 ticks/second * 60 seconds = 1200 ticks for 1 minute
+    private static final int TIMER_DURATION = 1200;
+
+    // A map to store a timer for each player
+    private static final HashMap<UUID, Integer> playerTimers = new HashMap<>();
 
     public static void registerEvents() {
         ServerTickEvents.END_SERVER_TICK.register(server -> {
             for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+                // Get the player's unique ID
+                UUID playerId = player.getUuid();
+                // Get the current timer value for this player, or 0 if they're not in the map yet
+                int timer = playerTimers.getOrDefault(playerId, 0);
+
+                // Only check crouching players
                 if (player.isSneaking()) {
-                    accelerateCropGrowth(player);
+                    // Increment the timer
+                    timer++;
+
+                    // Check if the timer has reached 1 minute
+                    if (timer >= TIMER_DURATION) {
+                        // Reset the timer for the next minute
+                        timer = 0;
+                        // Now, check for the 30% chance event
+                        handleTimedEvent(player);
+                    } else {
+                        // If the timer hasn't finished, run the normal growth logic
+                        runGrowthLogic(player);
+                    }
+                } else {
+                    // If the player is not sneaking, reset their timer
+                    timer = 0;
                 }
+                // Update the timer value in the map
+                playerTimers.put(playerId, timer);
             }
         });
     }
 
-    private static void accelerateCropGrowth(ServerPlayerEntity player) {
+    /**
+     * This method is called once per minute for a crouching player.
+     * It has a chance to trigger the special event.
+     */
+    private static void handleTimedEvent(ServerPlayerEntity player) {
         ServerWorld world = player.getWorld();
-
-        // Check if the special event should happen
+        // Check for the 30% chance
         if (world.getRandom().nextFloat() < FART_CHANCE) {
-            // FIX: Use the playSound method overload that takes coordinates instead of a BlockPos.
-            // This avoids the type conversion error.
-            world.playSound(
-                    null, // To all players
-                    player.getX(), // Player's X coordinate
-                    player.getY(), // Player's Y coordinate
-                    player.getZ(), // Player's Z coordinate
-                    SoundEvents.ENTITY_GENERIC_EXPLODE,
-                    SoundCategory.PLAYERS,
-                    0.5f, // Volume
-                    0.5f  // Pitch
-            );
+            // Play a low-pitched "fart" sound at the player's location
+            world.playSound(null, player.getX(), player.getY(), player.getZ(),
+                    SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.PLAYERS, 0.5f, 0.5f);
 
-            // Spawn some brown "dirt" particles around the player
+            // Spawn brown particles at the player's legs/bottom
             world.spawnParticles(
                     new BlockStateParticleEffect(ParticleTypes.FALLING_DUST, Blocks.DIRT.getDefaultState()),
                     player.getX(),
-                    player.getY() + 0.8, // Centered on the player's body
+                    player.getY() + 0.2, // Lower Y value to be near the legs
                     player.getZ(),
-                    30,    // Particle count
-                    0.3,   // X spread
-                    0.5,   // Y spread
-                    0.3,   // Z spread
-                    0.0    // Speed
-            );
-            // On this tick, no crop growth occurs.
+                    30, 0.2, 0.2, 0.2, 0.0);
         } else {
-            // If the special event doesn't happen, run the normal growth logic
-            BlockPos playerPos = player.getBlockPos();
-            for (int x = -GROWTH_RADIUS; x <= GROWTH_RADIUS; x++) {
-                for (int y = -1; y <= 1; y++) {
-                    for (int z = -GROWTH_RADIUS; z <= GROWTH_RADIUS; z++) {
-                        if (world.getRandom().nextInt(100) < GROWTH_CHANCE) {
-                            BlockPos cropPos = playerPos.add(x, y, z);
-                            BlockState cropState = world.getBlockState(cropPos);
+            // If the 30% chance fails, we still do a burst of growth for that minute of crouching
+            runGrowthLogic(player);
+        }
+    }
 
-                            if (cropState.getBlock() instanceof Fertilizable fertilizable) {
-                                if (fertilizable.isFertilizable(world, cropPos, cropState)) {
-                                    fertilizable.grow(world, world.getRandom(), cropPos, cropState);
-                                }
+    /**
+     * The logic for accelerating crop growth.
+     */
+    private static void runGrowthLogic(ServerPlayerEntity player) {
+        ServerWorld world = player.getWorld();
+        BlockPos playerPos = player.getBlockPos();
+        for (int x = -GROWTH_RADIUS; x <= GROWTH_RADIUS; x++) {
+            for (int y = -1; y <= 1; y++) {
+                for (int z = -GROWTH_RADIUS; z <= GROWTH_RADIUS; z++) {
+                    if (world.getRandom().nextInt(100) < GROWTH_CHANCE) {
+                        BlockPos cropPos = playerPos.add(x, y, z);
+                        BlockState cropState = world.getBlockState(cropPos);
+
+                        if (cropState.getBlock() instanceof Fertilizable fertilizable) {
+                            if (fertilizable.isFertilizable(world, cropPos, cropState)) {
+                                fertilizable.grow(world, world.getRandom(), cropPos, cropState);
                             }
                         }
                     }
